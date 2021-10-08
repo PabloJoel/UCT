73c158b11b81272d980634877697d83b59382725
Pablo Joel <pablojoel56@hotmail.com>
2021-10-08 19:53:51 +0200

    autoplay modification

--- autoplay.py
+++ autoplay.py
@@ -10,56 +10,63 @@ from games.CheckersGame import CheckersGame
 from players.RandomPlayer import RandomPlayer
 from players.UCTPlayer import UCTPlayer
 
-def write_res(iter, heur, lgr, white, black, draw):
-    if heur:
-        nombre = 'UCT vs UCT heur '+str(iter)+'.txt'
-    elif lgr:
-        nombre = 'UCT vs UCT lgr '+str(iter)+'.txt'
-    else:
-        nombre = 'UCT vs UCT '+str(iter)+'.txt'
+def play(player1_type, player2_type, iters1=None, iters2=None, heur1=None, heur2=None, lgr1=None, lgr2=None, choose_move1=None, choose_move2=None):
+    black = 0
+    white = 0
+    draw  = 0
+    i = 0
+    while(i<100):
+        player1 = create_player(player1_type, iters1, heur1, lgr1, choose_move1)
+        player2 = create_player(player2_type, iters2, heur2, lgr2, choose_move2)
+        
+        model = gm.GameModel(CheckersGame())
+        winner = model.play(player1=player1, player2=player2, show=False)
+
+        ret = return_info(player1, player2, iters1, iters2, heur1, heur2, lgr1, lgr2, choose_move1, choose_move2, winner, i)
+        print(ret)
+
+        if winner == 'Black':
+            black += 1
+        elif winner == 'White':
+            white += 1
+        elif winner == 'Draw':
+            draw += 1
+        i += 1
+
+    write_res(ret.replace(":"," "), black, white, draw)
+
+def write_res(title, black, white, draw):
+    nombre = title + '.txt'
 
     file = open(nombre,'a')
     file.write(f'White: {white}, Black: {black}, Draw: {draw}')
     file.close()
 
-white = 0
-black = 0
-draw = 0
-i = 0
+def create_player(player,iters,heur,lgr,choose):
+    if player=="rand":
+        return RandomPlayer(show=False)
+    else:
+        return UCTPlayer(game=CheckersGame(),iter=iters, choose=choose, heurs=heur,last_good_reply=lgr)
 
-mcts_iter1 = 700
-heur1 = False
-last_good_reply1 = False
+def return_info(player1, player2, iters1, iters2, heur1, heur2, lgr1, lgr2, choose_move1, choose_move2, winner, current_game_number):
+    prnt = str()
 
+    if player1=="rand":
+        prnt = prnt + 'Rand vs '
+    else:
+        prnt = prnt + 'UCT vs '
 
-mcts_iter2 = 700
-heur2 = True
-last_good_reply2 = False
-
-choose_move = 'max_value'
-
-while(i<100):
-    #player1 = UCTPlayer(game=CheckersGame(),iter=mcts_iter1,choose=choose_move, heurs=heur1,last_good_reply=last_good_reply1)
-    player1 = RandomPlayer(show=False)
-    player2 = UCTPlayer(game=CheckersGame(),iter=mcts_iter2,choose=choose_move, heurs=heur2,last_good_reply=last_good_reply2)
+    if player1=="rand":
+        prnt = prnt + 'Rand '
+    else:
+        prnt = prnt + 'UCT '
     
-    model = gm.GameModel(CheckersGame())
-    winner = model.play(player1=player1, player2=player2, show=False)
-    
-    print(f'Winner:{winner}, Current game: {i}, MCTS iter2: {mcts_iter2}, Heur2: {heur2}, Last Good Reply2: {last_good_reply2}, Choose:{choose_move}')
-    #print(f'Winner:{winner}, Current game: {i}, MCTS iter1: {mcts_iter1}, Heur1: {heur1}, Last Good Reply: {last_good_reply1}, MCTS iter2: {mcts_iter2}, Heur2: {heur2}, Last Good Reply2: {last_good_reply2}, Choose:{choose_move}')
-    
-    if winner == 'Black':
-        black += 1
-    elif winner == 'White':
-        white += 1
-    elif winner == 'Draw':
-        draw += 1
-    i += 1
+    prnt = f'Winner:{winner}, Current game: {current_game_number}'
+    if player1=="UCT":
+        prnt = prnt + f' MCTS iter1: {iters1}, Heur1: {heur1}, Last Good Reply1: {lgr1}, Choose Move1: {choose_move1}'
+    elif player2=="UCT":
+        prnt = prnt + f' MCTS iter2: {iters2}, Heur2: {heur2}, Last Good Reply2: {lgr2}, Choose Move2: {choose_move2}'
 
-total = black+white+draw
-print(f'Black:{black}, White:{white}, Draw:{draw}')
-print(f'Black:{(black/total)*100}, White:{(white/total)*100}, Draw:{(draw/total)*100}')
+    return prnt
 
-write_res(mcts_iter2, heur2, last_good_reply2, white, black, draw)
-
+play(player1_type="rand",player2_type="rand")
